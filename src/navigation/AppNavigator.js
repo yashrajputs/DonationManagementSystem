a0f2a1aa67616c0d68caadcd2e2eb4060fba2a13
Yash Rajput <yash@example.com>
2025-07-09 18:31:02 +0530

    Add ReportsScreen to navigation stack

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,6 +4,7 @@ import HomeScreen from '../screens/HomeScreen';
 import CampaignList from '../screens/CampaignList';
 import ProfileScreen from '../screens/ProfileScreen';
 import ProgressScreen from '../screens/ProgressScreen'; // Added ProgressScreen
+import ReportsScreen from '../screens/ReportsScreen'; // Import ReportsScreen
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +13,7 @@ const AppNavigator = () => (
     <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="Campaigns" component={CampaignList} />
     <Tab.Screen name="Progress" component={ProgressScreen} /> {/* Added Progress tab */}
+    <Tab.Screen name="Reports" component={ReportsScreen} /> {/* Added Reports tab */}
     <Tab.Screen name="Profile" component={ProfileScreen} />
   </Tab.Navigator>
 );
